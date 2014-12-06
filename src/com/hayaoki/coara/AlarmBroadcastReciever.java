efc7fc8621fac8afac0e20495b81b0c8ba415025
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2014-12-06 16:20:37 +0900

    こんな感じにやればアラームを設定できる?

---
+++ src/com/hayaoki/coara/AlarmBroadcastReciever.java
@@ -0,0 +1,25 @@
+package com.hayaoki.coara;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class AlarmBroadcastReciever extends BroadcastReceiver {
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    Toast.makeText(context, "Alarm Received!", Toast.LENGTH_SHORT).show();
+    Log.d("coara","received");
+  }
+
+}
